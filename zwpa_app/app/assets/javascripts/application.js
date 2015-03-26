1559e72835e411655a571febe6b2326cbeee52ea
Katherine Du <kndu@andrew.cmu.edu>
2015-03-26 17:07:24 -0400

    fixed datepicker so year doesn't show up twice

--- zwpa_app/app/assets/javascripts/application.js
+++ zwpa_app/app/assets/javascripts/application.js
@@ -27,8 +27,7 @@ $(function() {
   $(".datepicker").datepicker({
   	changeMonth: true,
     changeYear: true,
-    //format: 'mm/dd/YYYY'
-    dateFormat: "yyyy-mm-dd"
+    dateFormat: 'mm/dd/yy'
   });
 
 
